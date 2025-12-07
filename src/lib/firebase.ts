636c22aced0b7f18df66efb660267198a8bce729
Prakash8999 <jhaprakash504@gmail.com>
2025-12-08 00:21:11 +0530

    made changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "qwertyuioiuygtfvbnmkuygvbnmkiuhyg",
-  authDomain: "kanbanboard.firebaseapp.com",
-  projectId: "kanbanboard",
-  storageBucket: "kanbanboard.firebasestorage.app",
-  messagingSenderId: "745844752sd",
-  appId: "1:erthjklkjhb",
-  measurementId: "G-wertyuiklk"
+  apiKey: "AIzaSyCiNb39mA7eelA1LSvXT0alReQ12wPfo24",
+  authDomain: "kanbanboard-3ee63.firebaseapp.com",
+  projectId: "kanbanboard-3ee63",
+  storageBucket: "kanbanboard-3ee63.firebasestorage.app",
+  messagingSenderId: "951635947020",
+  appId: "1:951635947020:web:c3eb925c1e32b622660829",
+  measurementId: "G-7T09QVVYDR"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
